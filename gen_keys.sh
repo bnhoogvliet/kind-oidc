b5706cd026703b96eda750baffa173455534d11b
Beau Hoogvliet <beau.hoogvliet1@alliander.com>
2025-11-28 10:36:09 +0100

    cleanup

--- gen_keys.sh
+++
@@ -1,8 +0,0 @@
-# Generate the keypair
-PRIV_KEY="sa-signer.key"
-PUB_KEY="sa-signer.key.pub"
-PKCS_KEY="sa-signer-pkcs8.pub"
-# Generate a key pair
-ssh-keygen -t rsa -b 2048 -f $PRIV_KEY -m pem
-# convert the SSH pubkey to PKCS8
-ssh-keygen -e -m PKCS8 -f $PUB_KEY > $PKCS_KEY
--- go.mod
+++
@@ -1,8 +0,0 @@
-module example.com/m/v2
-
-go 1.25.4
-
-require (
-	github.com/go-jose/go-jose/v4 v4.1.3
-	github.com/pkg/errors v0.9.1
-)
